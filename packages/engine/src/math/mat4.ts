7d950d02a7efdc4f467f03430baec09b25918cd1
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-09 23:59:04 +0200

    feat(engine): add x-axis matrix rotation

--- packages/engine/src/math/mat4.ts
+++ packages/engine/src/math/mat4.ts
@@ -54,21 +54,21 @@ export class Mat4 {
     return out;
   }
 
-  static fromRotationZ(out: Mat4, radians: number): Mat4 {
+  static fromRotationX(out: Mat4, radians: number): Mat4 {
     const cosine = Math.cos(radians);
     const sine = Math.sin(radians);
     const r = out.values;
-    r[0] = cosine;
-    r[1] = sine;
+    r[0] = 1;
+    r[1] = 0;
     r[2] = 0;
     r[3] = 0;
-    r[4] = -sine;
+    r[4] = 0;
     r[5] = cosine;
-    r[6] = 0;
+    r[6] = sine;
     r[7] = 0;
     r[8] = 0;
-    r[9] = 0;
-    r[10] = 1;
+    r[9] = -sine;
+    r[10] = cosine;
     r[11] = 0;
     r[12] = 0;
     r[13] = 0;
@@ -92,6 +92,29 @@ export class Mat4 {
     r[8] = sine;
     r[9] = 0;
     r[10] = cosine;
+    r[11] = 0;
+    r[12] = 0;
+    r[13] = 0;
+    r[14] = 0;
+    r[15] = 1;
+    return out;
+  }
+
+  static fromRotationZ(out: Mat4, radians: number): Mat4 {
+    const cosine = Math.cos(radians);
+    const sine = Math.sin(radians);
+    const r = out.values;
+    r[0] = cosine;
+    r[1] = sine;
+    r[2] = 0;
+    r[3] = 0;
+    r[4] = -sine;
+    r[5] = cosine;
+    r[6] = 0;
+    r[7] = 0;
+    r[8] = 0;
+    r[9] = 0;
+    r[10] = 1;
     r[11] = 0;
     r[12] = 0;
     r[13] = 0;
