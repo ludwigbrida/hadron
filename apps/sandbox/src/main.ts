25517b39567f8b8267d1bb1760e615ada23cc7bd
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-09 23:31:28 +0200

    feat(sandbox): replace triangles with rotating cubes

--- apps/sandbox/src/main.ts
+++ apps/sandbox/src/main.ts
@@ -5,13 +5,19 @@ const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 
 const renderer = await Renderer.create(canvas);
 
-const triangle = {
-  positions: new Float32Array([0, 0.6, 0, -0.6, -0.6, 0, 0.6, -0.6, 0]),
-  indices: new Uint16Array([0, 1, 2]),
+const cube = {
+  positions: new Float32Array([
+    -0.25, -0.25, -0.25, 0.25, -0.25, -0.25, 0.25, 0.25, -0.25, -0.25, 0.25, -0.25, -0.25, -0.25,
+    0.25, 0.25, -0.25, 0.25, 0.25, 0.25, 0.25, -0.25, 0.25, 0.25,
+  ]),
+  indices: new Uint16Array([
+    0, 2, 1, 0, 3, 2, 4, 5, 6, 4, 6, 7, 0, 4, 7, 0, 7, 3, 1, 2, 6, 1, 6, 5, 0, 1, 5, 0, 5, 4, 3, 7,
+    6, 3, 6, 2,
+  ]),
 };
 
-const firstMesh = renderer.createMesh(triangle);
-const secondMesh = renderer.createMesh(triangle);
+const firstMesh = renderer.createMesh(cube);
+const secondMesh = renderer.createMesh(cube);
 const meshes = [firstMesh, secondMesh];
 
 firstMesh.setColor(new Float32Array([0.2, 0.7, 1, 1]));
@@ -23,16 +29,25 @@ const firstRotation = new Mat4();
 const secondTransform = new Mat4();
 const secondTranslation = new Mat4();
 const secondRotation = new Mat4();
+const projection = new Mat4();
 
-Mat4.fromTranslation(firstTranslation, 0.25, 0, 0);
-Mat4.fromTranslation(secondTranslation, -0.25, 0, 0);
+Mat4.fromTranslation(firstTranslation, 0.5, 0, -2);
+Mat4.fromTranslation(secondTranslation, -0.5, 0, -2);
+
+function updateProjection(): void {
+  Mat4.fromPerspective(projection, Math.PI / 3, canvas.clientWidth / canvas.clientHeight, 0.1, 100);
+  renderer.setViewProjection(projection);
+}
+
+new ResizeObserver(updateProjection).observe(canvas);
+updateProjection();
 
 function render(time: number): void {
-  Mat4.fromRotationZ(firstRotation, time / 1_000);
+  Mat4.fromRotationY(firstRotation, time / 1_000);
   Mat4.multiply(firstTransform, firstTranslation, firstRotation);
   firstMesh.setTransform(firstTransform);
 
-  Mat4.fromRotationZ(secondRotation, -time / 2_000);
+  Mat4.fromRotationY(secondRotation, -time / 2_000);
   Mat4.multiply(secondTransform, secondTranslation, secondRotation);
   secondMesh.setTransform(secondTransform);
 
