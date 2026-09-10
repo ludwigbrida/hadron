9012fcb2bbc5cd1f073feec50b9ee108d18d2d81
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-10 21:38:31 +0200

    feat(engine): add vector normalization

--- packages/engine/src/math/vec3.ts
+++ packages/engine/src/math/vec3.ts
@@ -1,5 +1,21 @@
 export class Vec3 extends Float32Array {
+  declare [0]: number;
+  declare [1]: number;
+  declare [2]: number;
+
   constructor(x: number, y: number, z: number) {
     super([x, y, z]);
+  }
+
+  normalize(): this {
+    const length = Math.hypot(this[0], this[1], this[2]);
+
+    if (length !== 0) {
+      this[0] /= length;
+      this[1] /= length;
+      this[2] /= length;
+    }
+
+    return this;
   }
 }
