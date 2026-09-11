a65bd8f06fece22a4c9453363f32d5e79ca00c6b
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-11 17:16:03 +0200

    refactor(engine): narrow public api exports

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,8 +1,4 @@
 export { Engine, type Frame, type UpdateCallback } from "./core/engine.ts";
-export { Input } from "./input/input.ts";
 export { Mat4 } from "./math/mat4.ts";
 export { Vec3 } from "./math/vec3.ts";
 export { Color } from "./rendering/color.ts";
-export { Geometry, type GeometryData } from "./rendering/geometry.ts";
-export { Camera } from "./scene/camera.ts";
-export { Scene } from "./scene/scene.ts";
