4feba3836581d32d2b2fc6f09014299b3d70c0ff
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-15 00:00:40 +0200

    feat(engine): add box colliders to bodies

--- packages/engine/src/physics/body.ts
+++ packages/engine/src/physics/body.ts
@@ -1,3 +1,11 @@
 import { Node } from "../scene/node.ts";
+import { BoxCollider, type BoxColliderOptions } from "./box-collider.ts";
 
-export abstract class Body extends Node {}
+export abstract class Body extends Node {
+  createBoxCollider(options: Readonly<BoxColliderOptions>): BoxCollider {
+    const collider = new BoxCollider(options);
+
+    this.addChild(collider);
+    return collider;
+  }
+}
--- packages/engine/src/physics/box-collider.ts
+++ packages/engine/src/physics/box-collider.ts
@@ -1,8 +1,16 @@
 import { Vector3 } from "../math/vector3.ts";
 import { Node } from "../scene/node.ts";
 
+export interface BoxColliderOptions {
+  readonly halfExtents: Readonly<Vector3>;
+}
+
 export class BoxCollider extends Node {
-  constructor(private readonly halfExtent: Readonly<Vector3>) {
+  readonly halfExtents: Vector3;
+
+  constructor(options: Readonly<BoxColliderOptions>) {
     super();
+
+    this.halfExtents = options.halfExtents.clone();
   }
 }
