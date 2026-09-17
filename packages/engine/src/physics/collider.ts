15e84915b3819dd42346651b95a69eae75010145
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-17 16:57:11 +0200

    feat(engine): add collider world position

--- packages/engine/src/physics/collider.ts
+++ packages/engine/src/physics/collider.ts
@@ -1,3 +1,12 @@
+import { Vector3 } from "../math/vector3.ts";
 import { Node } from "../scene/node.ts";
 
-export abstract class Collider extends Node {}
+export abstract class Collider extends Node {
+  // TODO: widen this method to parent class
+  getWorldPosition(): Vector3 {
+    const matrix = this.getWorldMatrix();
+
+    // TODO: realize transform reconstruction through matrix class
+    return new Vector3(matrix[12], matrix[13], matrix[14]);
+  }
+}
