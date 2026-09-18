828f13b1d6d8abaded8614d9a60d1fd35b17c65b
Ludwig Brida <ludwig@ludwigbrida.dev>
2026-09-18 18:47:18 +0200

    feat(engine): add vector3 max comparison setter

--- packages/engine/src/math/vector3.ts
+++ packages/engine/src/math/vector3.ts
@@ -71,6 +71,21 @@ export class Vector3 extends Float32Array {
   }
 
   /**
+   * Replaces each component with the larger component from this vector and another.
+   *
+   * @param vector The vector to compare against.
+   *
+   * @returns This vector after applying the maximum value comparison.
+   */
+  public max(vector: Readonly<Vector3>): this {
+    this[0] = Math.max(this[0], vector[0]);
+    this[1] = Math.max(this[1], vector[1]);
+    this[2] = Math.max(this[2], vector[2]);
+
+    return this;
+  }
+
+  /**
    * Sets the vector's components.
    *
    * @returns This vector.
